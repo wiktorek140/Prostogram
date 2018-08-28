1732c80534d3cdd62272b40293bc407303185617
Wiktor Strzębała <wiktorek140@tlen.pl>
2018-08-28 21:21:03 +0200

    Implement downloader for images. Rework connected with chached images
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>

--- src/cacheimage.cpp
+++ src/cacheimage.cpp
@@ -1,7 +1,6 @@
 #include "cacheimage.h"
 
-CacheImage::CacheImage(QObject *parent) : QObject(parent)
-{
+CacheImage::CacheImage(QObject *parent) : QObject(parent) {
 }
 
 void CacheImage::init() {
@@ -10,57 +9,75 @@ void CacheImage::init() {
     if(!c.exists()){
         c.mkdir(cacheLocation);
     }
-}
-
-QString CacheImage::getFromCache(const QString &str){
-    QUrl url(str);
-    path = cacheLocation+"/"+url.fileName();
-    if(QFile::exists(path) && QFile(path).size()>0)
-        return path;
-    else {
-        downloaded=false;
-        QDir::setCurrent(cacheLocation);
-        output.setFileName(url.fileName());
-        requestImage(url);
-        return path;
+    c.setPath(cacheLocation+"profilePic");
+    if(!c.exists()){
+        c.mkdir(cacheLocation+"profilePic");
     }
-}
-
-void CacheImage::requestImage(const QUrl &url){
-    currentDownload = manager.get(QNetworkRequest(QUrl(url)));
-    connect(currentDownload, &QIODevice::readyRead,
-            this, &CacheImage::downloadReadyRead);
-    connect(currentDownload, &QNetworkReply::finished,
-            this, &CacheImage::downloadFinished);
-
-
-    QEventLoop event;
-    connect(currentDownload,&QNetworkReply::finished,&event,&QEventLoop::quit);
-    event.exec();
-}
-
-void CacheImage::downloadFinished()
-{
-    downloaded=true;
-    output.close();
-    if (currentDownload->error()) {
-        qDebug() << "Error: " << currentDownload->errorString();
-    } else {
-        emit finishedImage();
-    }
-    currentDownload->deleteLater();
-}
-
-void CacheImage::downloadReadyRead()
-{
-    if(!output.isOpen()){output.open(QIODevice::WriteOnly);}
-    output.write(currentDownload->readAll());
 }
 
 void CacheImage::clean(){
     QDir::setCurrent(cacheLocation);
     QDir dir(cacheLocation);
-    if(dir.count()>100){
+    if(dir.count()>300){
         dir.removeRecursively();
     }
+    if(!dir.exists())dir.mkdir(cacheLocation);
+}
+
+
+QVariant CacheImage::getFromCache2(const QString &str) {
+    QUrl *url = new QUrl(str);
+    QString path = cacheLocation + "/" + url->fileName();
+    if(QFile::exists(path)) {
+        //qDebug()<< "File Exist - "<<url->toString();
+        return path;
+    }
+    else {
+        downloadManager.append(*url);
+        //qDebug()<< "File Requested - "<<url->toString();
+        return url->toString();
+    }
+}
+
+
+QVariant CacheImage::getFromCache(const QString &str,bool forceDownload) {
+    QUrl *url = new QUrl(str);
+    QString path = cacheLocation + "/" + url->fileName();
+
+    if(forceDownload) {
+        if(QFile::exists(path)) {
+            qDebug()<< "File Exist - Force remove - "<<url->toString();
+            QFile::remove(path);
+        }
+    } else {
+        if(QFile::exists(path)) {
+            qDebug()<< "File Exist - "<<url->toString();
+            return path;
+        }
+    }
+    downloadManager.append(*url);
+    //return path;
+    return url->toString();
+
+}
+
+QVariant CacheImage::getUserImageFromCache(const QString &str, bool forceDownload) {
+    QUrl url(str);
+    QString path = cacheLocation +"/profilePic/"+ url.fileName();
+
+    if(forceDownload) {
+        if(QFile::exists(path)) {
+            qDebug()<< "File Exist - Force remove - "<<url.toString();
+            QFile::remove(path);
+        }
+    } else {
+        if(QFile::exists(path)) {
+            qDebug()<< "File Exist - "<<url.toString();
+            return path;
+        }
+    }
+    downloadManager.append(url);
+    //return path;
+    return url.toString();
+
 }
--- src/cacheimage.h
+++ src/cacheimage.h
@@ -2,17 +2,13 @@
 #define CACHEIMAGE_H
 
 #include <QObject>
-#include <QStandardPaths>
 #include <QUrl>
 #include <QFile>
 #include <QDir>
 #include <QDebug>
 #include <QStandardPaths>
 
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QEventLoop>
+#include "downloadmanager.h"
 
 class CacheImage : public QObject
 {
@@ -21,25 +17,16 @@ public:
     explicit CacheImage(QObject *parent = 0);
 
     Q_INVOKABLE void init();
-    Q_INVOKABLE QString getFromCache(const QString &str);
     Q_INVOKABLE void clean();
-    Q_INVOKABLE void requestImage(const QUrl &url);
 
-    const QString cacheLocation = QStandardPaths::writableLocation(QStandardPaths::CacheLocation)+"/images";
+    Q_INVOKABLE QVariant getFromCache(const QString &str, bool forceDownload = false);
+    Q_INVOKABLE QVariant getUserImageFromCache(const QString &str, bool forceDownload = false);
+    Q_INVOKABLE QVariant getFromCache2(const QString &str);
 
-signals:
-    void finishedImage();
-
-public slots:
-    void downloadFinished();
-    void downloadReadyRead();
+    const QString cacheLocation = QStandardPaths::writableLocation(QStandardPaths::CacheLocation)+"/images/";
 
 private:
-    QNetworkAccessManager manager;
-    QNetworkReply *currentDownload;
-    QFile output;
-    bool downloaded=false;
-    QString path;
+    DownloadManager downloadManager;
 };
 
 #endif // CACHEIMAGE_H
